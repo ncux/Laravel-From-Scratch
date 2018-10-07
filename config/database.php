c4c2b334f36e1ca2ff0af6ec896dfd121a15d93e
Talent Malaba <malabatalent@gmail.com>
2018-10-07 14:35:02 +0800

    oops: had to remove that sensitive stuff

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 $url = parse_url(getenv("DATABASE_URL"));
-$host = $url['host']??null;
-$username = $url['user']??null;
-$password = $url['password']??null;
-$database = substr($url['path'], 1)??null;
+$host = $url['host'] ?? null;
+$username = $url['user'] ?? null;
+$password = $url['password'] ?? null;
+$database = substr($url['path'], 1) ?? null;
 
 
 
@@ -49,11 +49,11 @@ return [
 
         'mysql_deployed' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'development'),
-            'username' => env('DB_USERNAME', 'lowhanger'),
-            'password' => env('DB_PASSWORD', 'ma551ve9x'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
